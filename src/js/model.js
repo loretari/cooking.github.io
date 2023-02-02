ef6a1473479ed16fb2f2fc127a2d1f5cb17390ac
loretari <loretari@gmail.com>
2023-02-02 11:07:10 +0200

    init: implement pagination

--- src/js/model.js
+++ src/js/model.js
@@ -62,7 +62,8 @@ state.search.results = data.data.recipes.map(rec => {
 };
 
 export const getSearchResultsPage = function (page = state.search.page) {
+    state.search.page = page;
     const start = (page - 1) * state.search.resultsPerPage;
     const end = page * state.search.resultsPerPage;
     return state.search.results.slice(start, end);
-}
+};
